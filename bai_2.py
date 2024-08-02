d35c5e781b24e15cf84088d0bb8962baee661c31
Khanhbinhthuong <102637873+Bryan-David-123123@users.noreply.github.com>
2024-08-02 11:32:44 +0700

    Delete bai_2.py

--- bai_2.py
+++
@@ -1,5 +0,0 @@
-A={2023600333,2023000444,2023900555}
-B={2023900555,2023900666,2023900777}
-print("Sinh vien dang ky ca hai ban la:",A&B)
-print("Tong sinh vien dang ky ca hai ban la:",A|B)
-print("Sinh vien dang ky ban 1 ma khong dang ky ban 2 la:",A-B)
