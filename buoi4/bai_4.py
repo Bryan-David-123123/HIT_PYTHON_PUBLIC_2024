1eb5213c2f0306bb460c35e55483d707a2392cc6
KhanhNV <102637873+Bryan-David-123123@users.noreply.github.com>
2024-08-04 08:33:16 +0700

    Add files via upload

---
+++ buoi4/bai_4.py
@@ -0,0 +1,17 @@
+n=int(input())
+string_numbers=input()
+numbers=[int(num) for num in string_numbers.split()]
+numbers.sort()
+sum_max=int(input())
+sum=0
+i=0
+while i<n and sum+numbers[i]<=sum_max:
+    sum+=numbers[i]
+    i+=1
+s=set(numbers[:i])
+# s=[]
+# for index in range(i):
+#     # print(numbers[index],end=' ') 
+#     s.append(numbers[index])
+s=set(s)
+print(s)
