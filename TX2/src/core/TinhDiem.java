756d21d01284df56806888883041ea985ee1c7ca
KhanhNV <102637873+Bryan-David-123123@users.noreply.github.com>
2024-11-14 11:19:41 +0700

    Delete TX2 directory

--- TX2/src/core/TinhDiem.java
+++
@@ -1,5 +0,0 @@
-package core;
-
-public interface TinhDiem {
-	double tinhDiem();
-}
--- TX2/src/core/batloi.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-public class batloi extends Exception{
-	public batloi(String s) {
-		super(s);
-	}
-}
